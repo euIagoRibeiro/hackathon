440c7f72a0a472d12dba0e0cbc90920db514fdf0
Iago Ribeiro <iagoribeirospider2007dpr@gmail.com>
2024-12-04 14:56:58 -0300

    melhorando o visual do site

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile_menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.mobile-icon').src = "assets/menu.svg";
+        document.querySelector('.mobile-icon').src = "assets/people.svg";
     } else {
         menuMobile.classList.add('open');
         document.querySelector('.mobile-icon').src = "assets/close.svg";
