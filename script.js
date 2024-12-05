57fb3ef5c944328547ebf69c935baec7d6a9a1b1
euIagoRibeiro <iagoribeirospider2007dpr@gmail.com>
2024-12-05 03:57:01 -0300

    mudando totalmente o design do site

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ function menuShow() {
         document.querySelector('.mobile-icon').src = "assets/close.svg";
     }
 }
+
+function menuShow2() {
+    let menuMobile2 = document.querySelector('.mobile_menu2');
+    if (menuMobile2.classList.contains('open')) {
+        menuMobile2.classList.remove('open');
+        document.querySelector('.mobile-icon2').src = "assets/menu.svg";
+    } else {
+        menuMobile2.classList.add('open');
+        document.querySelector('.mobile-icon2').src = "assets/close.svg";
+    } 
+}
